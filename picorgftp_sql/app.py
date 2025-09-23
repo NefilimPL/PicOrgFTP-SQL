896de1a958bc9ff355b26580fa6c9bbbec22a187
NefilimPL <135364259+NefilimPL@users.noreply.github.com>
2025-09-23 13:41:24 +0200

    Import processed images directory path

--- picorgftp_sql/app.py
+++ picorgftp_sql/app.py
@@ -7,6 +7,7 @@ from .system_utils import get_file_lock_user, is_admin
 from .database import connect_db
 from .config import save_config
 from . import config, localization
+from .settings import l
 
 D = config.CONFIG
 LC = localization.LC
