caf814afc8bddcc27ea6ac10c4940389d8902cde
NefilimPL <135364259+NefilimPL@users.noreply.github.com>
2025-09-30 13:14:50 +0200

    Improve portable Python detection and hoisting

--- Dodatkowe (konwerter)/build_exe_portable.bat
+++ Dodatkowe (konwerter)/build_exe_portable.bat
@@ -26,8 +26,9 @@ set "TOOLSDIR=%REPO_ROOT%build-tools"
 set "PORTABLE_DIR=%TOOLSDIR%\python-portable"
 set "PORTABLE_CANON=%PORTABLE_DIR%"
 if "!PORTABLE_CANON:~-1!"=="\" set "PORTABLE_CANON=!PORTABLE_CANON:~0,-1!"
-set "PYTHON=%PORTABLE_DIR%\python.exe"
-if not exist "%PYTHON%" (
+
+call :DETECT_PORTABLE
+if not defined PYTHON (
     call :SETUP_PORTABLE || goto :ERROR
 )
 
@@ -40,6 +41,25 @@ if errorlevel 1 goto :ERROR
 echo.
 echo Done.
 popd >nul
+exit /b 0
+
+:DETECT_PORTABLE
+if not exist "%PORTABLE_DIR%\python.exe" call :RESCUE_PYTHON_EXE
+set "PYTHON="
+set "_CANDIDATE=%PORTABLE_DIR%\python.exe"
+if exist "%_CANDIDATE%" (
+    call :CHECK_TK_INTERPRETER "%_CANDIDATE%"
+    if defined PYTHON goto :DETECT_DONE
+)
+
+set "PYTHON="
+call :FIND_PYTHON "%PORTABLE_DIR%"
+if defined PYTHON (
+    set "_ALIGN_SRC=%PYTHON%"
+    call :ALIGN_PORTABLE "!_ALIGN_SRC!" || set "PYTHON="
+)
+
+:DETECT_DONE
 exit /b 0
 
 :SETUP_PORTABLE
@@ -58,29 +78,16 @@ set "INSTALLER_OPTS=/quiet InstallAllUsers=0 PrependPath=0 Include_launcher=0 In
 set "INSTALLER_OPTS=%INSTALLER_OPTS% TargetDir=""%PORTABLE_DIR%"" DefaultJustForMeTargetDir=""%PORTABLE_DIR%"" InstallLauncherAllUsers=0"
 "%TOOLSDIR%\%PYSETUP%" %INSTALLER_OPTS% || exit /b 1
 
-if not exist "%PYTHON%" (
-    set "PYTHON="
-    call :FIND_PYTHON "%PORTABLE_DIR%"  || goto :FALLBACK_ERROR
-    if not defined PYTHON (
-        if exist "%PORTABLE_DIR%\Scripts" call :FIND_PYTHON "%PORTABLE_DIR%\Scripts"  || goto :FALLBACK_ERROR
-    )
-    if not defined PYTHON if defined LOCALAPPDATA call :FIND_PYTHON "%LOCALAPPDATA%\Programs\Python"  || goto :FALLBACK_ERROR
+call :DETECT_PORTABLE
+if not defined PYTHON (
+    if defined LOCALAPPDATA call :FIND_PYTHON "%LOCALAPPDATA%\Programs\Python"  || goto :FALLBACK_ERROR
     if not defined PYTHON (
         echo Unable to locate python.exe inside %PORTABLE_DIR%.
         echo Make sure the installer can write to that directory and try again.
         exit /b 1
     )
-)
-
-:FOUND_PYTHON
-
-for %%d in ("%PYTHON%") do set "_FOUND_DIR=%%~dpd"
-set "_FOUND_CANON=!_FOUND_DIR!"
-if "!_FOUND_CANON:~-1!"=="\" set "_FOUND_CANON=!_FOUND_CANON:~0,-1!"
-if /i not "!_FOUND_CANON!"=="!PORTABLE_CANON!" (
-    echo Detected Python at !_FOUND_CANON!; copying files into %PORTABLE_DIR%...
-    call :COPY_INTERPRETER "!_FOUND_CANON!" || goto :FALLBACK_ERROR
-    set "PYTHON=%PORTABLE_DIR%\python.exe"
+    set "_ALIGN_SRC=%PYTHON%"
+    call :ALIGN_PORTABLE "!_ALIGN_SRC!" || goto :FALLBACK_ERROR
 )
 
 echo Installing pip and build dependencies...
@@ -94,6 +101,64 @@ del "%TOOLSDIR%\get-pip.py" >nul 2>nul
 del "%TOOLSDIR%\%PYSETUP%" >nul 2>nul
 
 "%PYTHON%" -c "import tkinter" >nul 2>nul || exit /b 1
+exit /b 0
+
+:ALIGN_PORTABLE
+set "_ALIGN_SRC=%~1"
+if not defined _ALIGN_SRC exit /b 1
+for %%d in ("%_ALIGN_SRC%") do set "_FOUND_DIR=%%~dpd"
+set "_FOUND_CANON=!_FOUND_DIR!"
+if "!_FOUND_CANON:~-1!"=="\" set "_FOUND_CANON=!_FOUND_CANON:~0,-1!"
+if /i "!_FOUND_CANON!"=="!PORTABLE_CANON!" (
+    set "PYTHON=%PORTABLE_DIR%\python.exe"
+    if not exist "%PYTHON%" (
+        set "PYTHON=%_ALIGN_SRC%"
+    )
+    exit /b 0
+)
+set "_FOUND_CANON_UP=!_FOUND_CANON!"
+call :TOUPPER _FOUND_CANON_UP
+set "_PORTABLE_UP=!PORTABLE_CANON!"
+call :TOUPPER _PORTABLE_UP
+set "_PORTABLE_PREFIX=!_PORTABLE_UP!\"
+set "_RELATIVE=!_FOUND_CANON_UP:%_PORTABLE_PREFIX%=!"
+if not "!_RELATIVE!"=="!_FOUND_CANON_UP!" (
+    call :HOIST_INTERPRETER "!_FOUND_CANON!" || exit /b 1
+    set "PYTHON=%PORTABLE_DIR%\python.exe"
+    exit /b 0
+)
+
+echo Detected Python at !_FOUND_CANON!; copying files into %PORTABLE_DIR%...
+call :COPY_INTERPRETER "!_FOUND_CANON!" || exit /b 1
+set "PYTHON=%PORTABLE_DIR%\python.exe"
+exit /b 0
+
+:HOIST_INTERPRETER
+set "_SRC_DIR=%~1"
+if not defined _SRC_DIR exit /b 1
+if /i "!_SRC_DIR!"=="!PORTABLE_CANON!" exit /b 0
+robocopy "%_SRC_DIR%" "%PORTABLE_DIR%" /e >nul
+set "RC=%errorlevel%"
+if %RC% GEQ 8 exit /b 1
+if exist "%_SRC_DIR%" rmdir /s /q "%_SRC_DIR%"
+if not exist "%PORTABLE_DIR%\python.exe" exit /b 1
+exit /b 0
+
+:RESCUE_PYTHON_EXE
+for %%f in ("%PORTABLE_DIR%\python-*.exe") do (
+    if exist %%~ff (
+        ren "%%~ff" python.exe >nul 2>nul
+        if not errorlevel 1 exit /b 0
+    )
+)
+exit /b 0
+
+:TOUPPER
+set "_VAR=%~1"
+set "_VALUE=!%~1!"
+if not defined _VALUE exit /b 0
+for %%A in (a b c d e f g h i j k l m n o p q r s t u v w x y z) do set "_VALUE=!_VALUE:%%A=%%A!"
+set "%_VAR%=%_VALUE%"
 exit /b 0
 
 :FALLBACK_ERROR
