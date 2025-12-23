927e9dfa555e8780f73b03ee5f0f343b63110588
NefilimPL <135364259+NefilimPL@users.noreply.github.com>
2025-12-23 20:14:34 +0100

    Adjust PyInstaller mysql imports

--- Dodatkowe (konwerter)/build.ps1
+++ Dodatkowe (konwerter)/build.ps1
@@ -18,12 +18,16 @@ python -m pip install -r requirements-build.txt
 
 # build
 # --collect-data mysql.connector  # włącz pojedynczo, aby ustalić blokującą flagę
-# --collect-submodules mysql.connector  # włącz pojedynczo po --collect-data
 # --exclude-module pyodbc  # tymczasowo, jeśli build nadal się blokuje
 pyinstaller --noconfirm --clean --log-level=DEBUG `
   --name PicOrgFTP-SQL `
   --debug=imports `
   --noconsole `
+  --collect-data mysql.connector `
+  --hidden-import mysql.connector `
+  --hidden-import mysql.connector.connection `
+  --hidden-import mysql.connector.cursor `
+  --hidden-import mysql.connector.errors `
   --add-data "picorgftp_sql/Localization;picorgftp_sql/Localization" `
   PicOrgFTP-SQL.pyw
 
