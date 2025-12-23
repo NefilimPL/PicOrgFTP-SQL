31fdd4a6a98362ad6e3b00da9032df8e3bd7efec
NefilimPL <135364259+NefilimPL@users.noreply.github.com>
2025-12-23 19:25:46 +0100

    Update build.ps1

--- Dodatkowe (konwerter)/build.ps1
+++ Dodatkowe (konwerter)/build.ps1
@@ -17,15 +17,17 @@ python -m pip install "pyinstaller>=6.6,<7"
 python -m pip install -r requirements-build.txt
 
 # build
+# --collect-data mysql.connector  # włącz pojedynczo, aby ustalić blokującą flagę
+# --collect-submodules mysql.connector  # włącz pojedynczo po --collect-data
+# --exclude-module pyodbc  # tymczasowo, jeśli build nadal się blokuje
 pyinstaller --noconfirm --clean --log-level=DEBUG `
   --name PicOrgFTP-SQL `
   --debug=imports `
   --noconsole `
   --add-data "picorgftp_sql/Localization;picorgftp_sql/Localization" `
-  # --collect-data mysql.connector ` # włącz pojedynczo, aby ustalić blokującą flagę
-  # --collect-submodules mysql.connector ` # włącz pojedynczo po --collect-data
-  # --exclude-module pyodbc ` # tymczasowo, jeśli build nadal się blokuje
   PicOrgFTP-SQL.pyw
 
+
 Write-Host "OK. Wynik: dist\PicOrgFTP-SQL\"
+
 
