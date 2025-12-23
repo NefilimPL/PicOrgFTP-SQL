9f4fec191898f5f2e61b672d64624befdc138e28
NefilimPL <135364259+NefilimPL@users.noreply.github.com>
2025-12-23 17:23:55 +0100

    Add files via upload

---
+++ Dodatkowe (konwerter)/build.ps1
@@ -0,0 +1,27 @@
+﻿# build.ps1
+$ErrorActionPreference = "Stop"
+Set-Location $PSScriptRoot
+
+# venv
+if (!(Test-Path ".\.venv")) {
+    py -3.11 -m venv .venv
+}
+
+# activate venv
+.\.venv\Scripts\Activate.ps1
+
+# deps
+python -m pip install --upgrade pip
+python -m pip install "pyinstaller>=6.6,<7"
+python -m pip install -r requirements-build.txt
+
+# build
+pyinstaller --noconfirm --clean --log-level=WARN `
+  --name PicOrgFTP-SQL `
+  --noconsole `
+  --add-data "picorgftp_sql/Localization;picorgftp_sql/Localization" `
+  --collect-submodules mysql.connector `
+  --collect-data mysql.connector `
+  PicOrgFTP-SQL.pyw
+
+Write-Host "OK. Wynik: dist\PicOrgFTP-SQL\"
