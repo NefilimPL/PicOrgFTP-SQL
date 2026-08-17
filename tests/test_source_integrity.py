7e2b90067de7a4316117900e3cef85411bc9ad48
NefilimPL <135364259+NefilimPL@users.noreply.github.com>
2026-08-17 10:37:43 +0200

    test: update web asset cache keys

--- tests/test_source_integrity.py
+++ tests/test_source_integrity.py
@@ -99,9 +99,9 @@ class SourceIntegrityTests(unittest.TestCase):
         self.assertIsNotNone(css_match)
         self.assertIsNotNone(runtime_js_match)
         self.assertIsNotNone(js_match)
-        self.assertEqual(css_match.group(1), "20260724-list-usage-guard1")
+        self.assertEqual(css_match.group(1), "20260817-pimcore-export-layout-css1")
         self.assertEqual(runtime_js_match.group(1), "20260728-runtime-poll1")
-        self.assertEqual(js_match.group(1), "20260727-product-query1")
+        self.assertEqual(js_match.group(1), "20260817-pimcore-export-layout-js1")
         self.assertNotEqual(css_match.group(1), js_match.group(1))
 
     def test_resource_detail_copy_explains_clients_and_latch_stages(self) -> None:
